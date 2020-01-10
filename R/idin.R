925b1411c6161025bda6e8a586a207de91202450
Michael Culshaw-Maurer <mjculshawmaurer@ucdavis.edu>
2020-01-10 15:45:14 -0800

    fixed function so it only changes behavior if both args are numeric

--- R/idin.R
+++ R/idin.R
@@ -1,4 +1,4 @@
-#' replacing the %in% function with the all.equal equivalent
+#' this is R's original %in% function
 #'
 #' @param x: vector for which you're looking for matches
 #' @param y: vector you're looking for matches in
@@ -10,11 +10,5 @@
 #' @examples
 #' x <- seq(from = 1, to = 3, by = 0.1)
 #' y <- c(3, 2.4, 1.8, 1.4)
-#' x %in% y
-`%in%` <- function(x,y, tol = sqrt(.Machine$double.eps)) {
-  out <- logical(length(x))
-  for(i in 1:length(x)) out[i] <- any(abs(x[i] - y) <= tol)
-  out
-}
-
-#`%idin%` <- function(x, table) match(x, table, nomatch = 0) > 0
+#' x %idin% y
+`%idin%` <- function(x, table) match(x, table, nomatch = 0) > 0
--- R/in.R
+++ R/in.R
@@ -1,4 +1,4 @@
-#' this is R's original %in% function
+#' replacing the %in% function with the all.equal equivalent
 #'
 #' @param x: vector for which you're looking for matches
 #' @param y: vector you're looking for matches in
@@ -10,5 +10,15 @@
 #' @examples
 #' x <- seq(from = 1, to = 3, by = 0.1)
 #' y <- c(3, 2.4, 1.8, 1.4)
-#' x %idin% y
-`%idin%` <- function(x, table) match(x, table, nomatch = 0) > 0
+#' x %in% y
+`%in%` <- function(x,y, tol = sqrt(.Machine$double.eps)) {
+  if (is.numeric(x) & is.numeric(y)) {
+    out <- logical(length(x))
+    for(i in 1:length(x)) out[i] <- any(abs(x[i] - y) <= tol)
+    out
+  } else{
+    match(x, y, nomatch = 0) > 0
+  }
+
+}
+
