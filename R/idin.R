f7b65a0e18ed777bed0cf443a4c8ab1dac8970db
Michael Culshaw-Maurer <mjculshawmaurer@ucdavis.edu>
2020-01-10 20:33:03 -0800

    fixed the documentation formatting

--- R/idin.R
+++ R/idin.R
@@ -1,14 +1,12 @@
-#' this is R's original %in% function
+#' this is R's original \%in\% function
 #'
-#' @param x: vector for which you're looking for matches
-#' @param y: vector you're looking for matches in
-#'
-#'
+#' @param x a vector whose values are being evaluated for matches
+#' @param y a vector whose values x is looking for matches in
 #' @return A logical vector of length x
-#' @export
-#'
 #' @examples
 #' x <- seq(from = 1, to = 3, by = 0.1)
 #' y <- c(3, 2.4, 1.8, 1.4)
 #' x %idin% y
-`%idin%` <- function(x, y) match(x, y, nomatch = 0) > 0
+`%idin%` <- function(x, y){
+  match(x, y, nomatch = 0) > 0
+}
--- R/in.R
+++ R/in.R
@@ -1,16 +1,15 @@
-#' replacing the %in% function with the all.equal equivalent
+#' replacing the \%in\% function with the all.equal equivalent
 #'
-#' @param x: vector for which you're looking for matches
-#' @param y: vector you're looking for matches in
-#'
-#'
+#' @param x a vector whose values are being evaluated for matches
+#' @param y a vector whose values x is looking for matches in
+#' @param tol the tolerance for differences between values of x and y to still be considered a match
 #' @return A logical vector of length x
-#' @export
-#'
 #' @examples
 #' x <- seq(from = 1, to = 3, by = 0.1)
 #' y <- c(3, 2.4, 1.8, 1.4)
 #' x %in% y
+#'
+
 `%in%` <- function(x,y, tol = sqrt(.Machine$double.eps)) {
   if (is.numeric(x) & is.numeric(y)) {
     out <- logical(length(x))
