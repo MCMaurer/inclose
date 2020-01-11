62baccfa8f127a6f99a742b9c8cbb774582b38ff
Michael Culshaw-Maurer <mjculshawmaurer@ucdavis.edu>
2020-01-11 13:05:06 -0800

    fixed a lot of documentation issues

--- R/idin.R
+++ R/idin.R
@@ -1,5 +1,8 @@
-#' this is R's original \%in\% function
+#' Rewrite of R's original \%in\% function
 #'
+#' This is the same as R's original \code{\link[base]{\%in\%}}, but written as shorthand for "identical in".
+#'
+#' @export
 #' @param x a vector whose values are being evaluated for matches
 #' @param y a vector whose values x is looking for matches in
 #' @return A logical vector of length x
@@ -7,6 +10,7 @@
 #' x <- seq(from = 1, to = 3, by = 0.1)
 #' y <- c(3, 2.4, 1.8, 1.4)
 #' x %idin% y
+#'
 `%idin%` <- function(x, y){
   match(x, y, nomatch = 0) > 0
 }
--- R/in.R
+++ R/in.R
@@ -1,14 +1,20 @@
-#' replacing the \%in\% function with the all.equal equivalent
+#' \%in\% with tolerance
 #'
-#' @param x a vector whose values are being evaluated for matches
-#' @param y a vector whose values x is looking for matches in
-#' @param tol the tolerance for differences between values of x and y to still be considered a match
+#' This function replaces R's default \code{\link[base]{\%in\%}} function with an \code{\link[base]{all.equal}} equivalent. It is slower than the default \code{\link[base]{\%in\%}}, but it is comparable to the speed differences between \code{\link[base]{identical}} and \code{\link[base]{all.equal}}. It takes a lefthand side (LHS) and righthand side (RHS) vector, evaluates whether each value on the LHS is found somewhere in the RHS, within the matching tolerance, and returns a logical vector of length(LHS).
+#'
+#' @export
+#' @param x a vector whose values are being evaluated for matches in y
+#' @param y a vector whose values x is being evaluated for matches in
+#' @param tol the tolerance for differences between values of x and y to still be considered a match, by default set to \code{.Machine$double.eps}
 #' @return A logical vector of length x
 #' @examples
 #' x <- seq(from = 1, to = 3, by = 0.1)
 #' y <- c(3, 2.4, 1.8, 1.4)
 #' x %in% y
 #'
+#' # If you want a different tolerance value you need
+#' # to use the standard function form:
+#' `%in%`(x, y, tol = 0.00001)
 
 `%in%` <- function(x,y, tol = sqrt(.Machine$double.eps)) {
   if (is.numeric(x) & is.numeric(y)) {
